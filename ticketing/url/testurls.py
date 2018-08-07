8ec5c47c46fadbe7389e5245478d0357b7a86f80
Unknown <dominic@dbp.kiwi>
2018-08-07 13:46:03 +1200

    Removed unnecessary code
    - The code was pointed out by Hayden

--- ticketing/url/testurls.py
+++ ticketing/url/testurls.py
@@ -1,7 +1,1 @@
 from django.conf.urls import url
-
-from ticketing.views import testsign
-
-urlpatterns = [
-    url(r'^runsign/', testsign, name="driverauthtokenview"),
-]
--- ticketing/views.py
+++ ticketing/views.py
@@ -3,14 +3,3 @@ from django.http import JsonResponse
 from ticketing.models import BalanceTicket
 
 # Create your views here.
-from ticketing.userticket.createqrcode import QRCode
-
-
-def testsign(request):
-    bt = BalanceTicket.objects.get(id=1)
-
-    creator = QRCode()
-    qr = creator.createbtqrcode(bt)
-    print(qr)
-    print(creator.verify(qr))
-    return HttpResponse("yes")
