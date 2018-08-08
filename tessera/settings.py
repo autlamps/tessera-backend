14171b7b58302e389bd7af60ecc894ddc6cb6a02
Hayden Woodhead <hayden.sw@icloud.com>
2018-08-08 15:56:07 +1200

    Added pgsql dependency

--- tessera/settings.py
+++ tessera/settings.py
@@ -127,8 +127,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
-STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 PUBLIC_KEY = os.environ.get('PUBLIC_KEY', '')
 PRIVATE_KEY = os.environ.get('PRIVATE_KEY', '')
