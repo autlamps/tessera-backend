4d2aa2f202af4131b3db7dd7ba5ff0cb5d271d94
Hayden Woodhead <hayden.sw@icloud.com>
2018-08-08 16:31:37 +1200

    Add ability to set hostname via env var

--- tessera/settings.py
+++ tessera/settings.py
@@ -31,7 +31,7 @@ if SECRET_KEY == '':
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', os.environ.get('HOSTNAME', '')]
 
 
 # Application definition
