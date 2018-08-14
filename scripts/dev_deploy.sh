7aee9e8962355be0093260fbcb30ea15576714a6
Hayden Woodhead <hayden.sw@icloud.com>
2018-08-14 14:27:39 +1200

    Switched key to file rather than env var

--- scripts/dev_deploy.sh
+++ scripts/dev_deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # adapted from http://blog.abarbanell.de/linux/2017/09/09/deploy-from-travis-to-dokku/
-echo "$SSH_PRIVATE_KEY" >> deploy_key
-chmod 600 deploy_key
 eval "$(ssh-agent -s)"
+chmod 600 .travis/deploy.key
+ssh-add .travis/deploy.key
 echo -ne '\n' | ssh-add deploy_key
 ssh-keyscan tessera-dev.haydenwoodhead.com >> ~/.ssh/known_hosts
 git remote add deploy dokku@tessera-dev.haydenwoodhead.com:dev-api
