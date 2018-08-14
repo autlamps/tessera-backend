b04d24b9c48236e4657a129804d8067125477370
Hayden Woodhead <hayden.sw@icloud.com>
2018-08-14 12:44:36 +1200

    Fixed errors in deploy script

--- scripts/dev_deploy.sh
+++ scripts/dev_deploy.sh
@@ -3,8 +3,8 @@
 echo "$SSH_PRIVATE_KEY" >> deploy_key
 chmod 600 deploy_key
 eval "$(ssh-agent -s)"
-ssh-add deploy_Key
-ssh-keyscan tessera-dev.haydenwoodhead.com >> >> ~/.ssh/known_hosts
+ssh-add deploy_key
+ssh-keyscan tessera-dev.haydenwoodhead.com >> ~/.ssh/known_hosts
 git remote add deploy dokku@tessera-dev.haydenwoodhead.com:dev-api
 git config --global push.default simple
-git push deploy dev:master
+git push --force deploy dev:master
