c63da3ae74aa135c067215ef5e61159bfd381b2a
izaac <izaacmarccrooke16@gmail.com>
2018-09-25 11:50:06 +1200

    corrections for travis

--- ticketing/url/managementurl.py
+++ ticketing/url/managementurl.py
@@ -3,5 +3,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': 'users:login'}, name='logout'),
+    url(r'^logout/$', auth_views.logout, {'next_page':
+                         'users:login'}, name='logout'),
 ]
--- ticketing/views/logout.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth import logout
-
-def logout_view(request):
-    logout(request)
-    #redirect to success page
-
