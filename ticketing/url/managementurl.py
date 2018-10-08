bfa0e5f4a86d1a1b3441e36f79c577f867ec6082
izaac <izaacmarccrooke16@gmail.com>
2018-10-09 12:12:46 +1300

    working login page

--- ticketing/url/managementurl.py
+++ ticketing/url/managementurl.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page':
-                         'users:login'}, name='logout'),
+                         'management:login'}, name='logout'),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
 ]
